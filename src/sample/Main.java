f5875fe2ac937e9c9b391ff4955c24b73710a05a
Kyle <kderrick1988@gmail.com>
2016-11-08 12:37:24 -0800

    initial files for first Java FX app

---
+++ src/sample/Main.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Group root = new Group();
+//        Label label = new Label("Name:");
+//        Text txt  = new Text("Sup?");
+//        txt.setFont(new Font("Papyrus", 80));
+//        TextField nameField = new TextField();
+//        GridPane grid = new GridPane();
+//        grid.add(label, 0, 0);
+//        Button btn = new Button();
+//        grid.add(nameField, 1, 0);
+//        grid.add(btn, 1, 1);
+//        grid.setHgap(20);
+////        grid.setGridLinesVisible(true);
+//        btn.setText("Say Sup?");
+//        btn.setOnAction(evt -> System.out.printf("Sup %s! %n", nameField.getText()));
+//        VBox box = new VBox();
+//        txt.setY(50);
+//        box.getChildren().addAll(txt, grid);
+//        root.getChildren().add(box);
+        primaryStage.setTitle("Sup");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
